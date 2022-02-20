3aedb87ccd800460a2726c208c0615a062e3a6eb
the-groot <sangwoo0795@naver.com>
2022-02-20 19:52:54 +0900

    chapter10 upload

---
+++ Chapter10/TryCatchFinallyExample.java
@@ -0,0 +1,14 @@
+package Chapter10;
+
+public class TryCatchFinallyExample {
+    public static void main(String args[]){
+        System.out.println("엥");
+        try{
+            Class clazz=Class.forName("java.lang.String2");
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("클래스가 존재하지 않습니다.");
+        }
+    }
+
+}
